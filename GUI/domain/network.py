45bf860792728a23bd46d0613eb0a380d82c85d8
mwtegelaers <mwtegelaers@gmail.com>
2014-03-20 09:31:20 +0100

    added direction to local network manager.

--- GUI/domain/network.py
+++ GUI/domain/network.py
@@ -1,9 +1,8 @@
 import SenderGUI
 from util.decorators import singleton
+from util.values import TEAMS, OUR_TEAM
 from ReceiverGUI import receive
 #from SenderGUI import SenderGUI
-
-TEAM = 'geel'
 
 
 @singleton
@@ -19,8 +18,19 @@ class NetworkManager(object):
 
         #self._sender = SenderGUI()
         #receive(self)
-        self._duckduck = {"geel": "yellow",
-                          "rood": "red"}
+        self.nl_to_en = {"geel"     : "yellow",
+                         "paars"    : "purple",
+                         "blauw"    : "blue",
+                         "rood"     : "red",
+                         "groen"    : "green",
+                         "wit"      : "white",
+                         "indigo"   : "indigo",
+                         "brons"    : "bronze",
+                         "silver"   : "silver",
+                         "goud"     : "gold",
+                         "koper"    : "copper",
+                         "platinum" : "platinum",
+                         "ijzer"    : "iron"}
 
     @property
     def zeppelin_cat(self):
@@ -33,25 +43,30 @@ class NetworkManager(object):
     #-----------------------------------------------------------------------------
     # Setters | Interface for network
     def set_position(self, team, new_pos):
-        self._check_team(self._duckduck[team])
-        self.zeppelin_cat[self._duckduck[team]].pos = new_pos
+        self._check_team(self.nl_to_en[team])
+        self.zeppelin_cat[self.nl_to_en[team]].pos = new_pos
 
     def set_height(self, team, new_height):
-        self._check_team(self._duckduck[team])
-        self.zeppelin_cat[self._duckduck[team]].height = new_height
+        self._check_team(self.nl_to_en[team])
+        self.zeppelin_cat[self.nl_to_en[team]].height = new_height
 
     def set_goal_pos(self, team, new_goal_pos):
-        self._check_team(self._duckduck[team])
-        if self._duckduck[team] == TEAM:
-            self.zeppelin_cat[self._duckduck[team]].goal_pos = new_goal_pos
+        self._check_team(self.nl_to_en[team])
+        if self.nl_to_en[team] == OUR_TEAM:
+            self.zeppelin_cat[self.nl_to_en[team]].goal_pos = new_goal_pos
 
     def set_goal_height(self, team, new_goal_height):
-        self._check_team(self._duckduck[team])
-        if self._duckduck[team] == TEAM:
-            self.zeppelin_cat[self._duckduck[team]].goal_height = new_goal_height
+        self._check_team(self.nl_to_en[team])
+        if self.nl_to_en[team] == OUR_TEAM:
+            self.zeppelin_cat[self.nl_to_en[team]].goal_height = new_goal_height
+
+    def set_direction(self, team, new_direction):
+        self._check_team(team)
+        self._check_team(self.nl_to_en[team])
+        if self.nl_to_en[team] == OUR_TEAM:
+            self.zeppelin_cat[self.nl_to_en[team]].direction = new_direction
 
     def add_to_console(self, new_str):
-        "print adding to console"
         self.console_buffer.add(new_str)
 
     #-----------------------------------------------------------------------------
@@ -65,17 +80,23 @@ class NetworkManager(object):
         return self.zeppelin_cat[team].height
 
     def get_goal_pos(self, team):
-        if team == TEAM:
+        if team == OUR_TEAM:
             self._check_team(team)
             return self.zeppelin_cat[team].goal_pos
 
     def get_goal_height(self, team):
-        if team == TEAM:
+        if team == OUR_TEAM:
             self._check_team(team)
             return self.zeppelin_cat[team].goal_height
 
+    def get_direction(self, team):
+        if team == OUR_TEAM:
+            self._check_team(team)
+            return self.zeppelin_cat[team].direction
+
     def get_console(self):
         return self.console_buffer.get_and_clean()
+
 
     #-------------------------------------------------------------------------
     # Senders | Interface for GUI
@@ -87,7 +108,7 @@ class NetworkManager(object):
 
     def _check_team(self, team):
         if not team in self.zeppelin_cat:
-            if team != TEAM:
+            if team != OUR_TEAM:
                 self.zeppelin_cat[team] = NetworkZeppelinView()
             else:
                 self.zeppelin_cat[team] = NetworkZeppelinViewYellow()
@@ -116,15 +137,16 @@ class NetworkZeppelinView(object):
 
 
 class NetworkZeppelinViewYellow(NetworkZeppelinView):
-    def __init__(self, pos=[0, 0], height=1000, goal_pos=[0, 0], goal_height=1000):
+    def __init__(self, pos=[0, 0], height=1000, goal_pos=[0, 0], goal_height=1000, direction=0):
         super(NetworkZeppelinViewYellow, self).__init__(pos, height)
 
         self._goal_height = goal_height
         self._goal_pos = goal_pos
+        self._direction = direction
 
     @property
     def goal_height(self):
-        self._goal_height
+        return self._goal_height
 
     @goal_height.setter
     def goal_height(self, new_goal_height):
@@ -137,6 +159,14 @@ class NetworkZeppelinViewYellow(NetworkZeppelinView):
     @goal_pos.setter
     def goal_pos(self, new_goal_pos):
         self._goal_pos = new_goal_pos
+
+    @property
+    def direction(self):
+        return self._direction
+
+    @direction.setter
+    def direction(self, new_direction):
+        self._direction = new_direction
 
 
 class NetworkConsoleInfo(object):
